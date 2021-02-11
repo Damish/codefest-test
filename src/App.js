763c63500a57794bfaeb7c75a20067902d2cc05d
Damish <damishs88@gmail.com>
2021-02-11 23:31:05 +0530

    correct folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import Home from "./components/home";
+import Home from "./components/homeComponent/home";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from "./components/Login";
-import View from "./components/view";
+import Login from "./components/loginComponent/Login";
+import View from "./components/viewComponent/view";
 function App() {
   return (
     <div>
