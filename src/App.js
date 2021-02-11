17b5f35393d7953cb6e1c30506d0f0bd605ec322
sathiralamal <sdsat756@gmail.com>
2021-02-11 22:32:11 +0530

    add login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Home from "./components/home";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Login from "./components/Login";
 function App() {
   return (
     <div>
       <Home/>
+      <Login/>
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+
+class Login extends Component {
+    render() {
+        return (
+            <Container>
+                Login
+            </Container>
+        );
+    }
+}
+
+export default Login;
